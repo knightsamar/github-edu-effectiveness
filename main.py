feb6ce1e2f160fae743ea24a5e4b7c6fac50f0a9
Samarendra M Hedaoo <samarendra@fortyplustwo.net>
2015-03-25 10:25:19 +0530

    Now we use PyGitHub library for easier and better retrieval.
    Also using a bit of OOP.

--- main.py
+++ main.py
@@ -1,9 +1,61 @@
-from utils import GitHub
+from github import Github
+from utils import get_all_related_repos, get_all_commits_on_repos, get_all_pull_requests
+import csv
 
-#forks of https://api.github.com/repos/gayatrivenugopal/MT_A_Feedback
-g = GitHub()
-g.get_forks("https://api.github.com/repos/gayatrivenugopal/MT_A_Feedback/forks")
-print g.repos
+class GithubInfo:
+    r = None
+    repos = {}
+    commits = {}
+    pull_requests = {}
 
-g.commits_on_repos(g.repos)
+    def __init__(self, user = 'gayatrivenugopal', repo = "MobileTechnologies"):
+        self.r = g.get_user(user).get_repo(repo)
+
+    def forks_info(self):
+        self.repos = get_all_related_repos(self.r)
+        with open('forks.csv','w') as fcsv:
+            fieldnames = ['owner', 'repo']
+            w = csv.DictWriter(fcsv, fieldnames)
+
+            w.writeheader()
+            for o,r in self.repos.items():
+                w.writerow({'owner':o, 'repo':r.name})
+
+            fcsv.flush()
+
+    def commits_info(self):
+        self.commits = get_all_commits_on_repos(self.repos)
+
+        with open('commits.csv','w') as ccsv:
+            fieldnames = ['sha', 'author', 'committer', 'repo','repo_url', 'date', 'comment',]
+            w = csv.DictWriter(ccsv, fieldnames)
         
+            w.writeheader()
+            for sha,commit in self.commits.items():
+                w.writerow({'sha': sha, 'author':commit['author'], 'committer': commit['committer'], 'repo' : commit['repo'], 'repo_url' : commit['repo_url'], 'date': commit['date'], 'comment': commit['comment']})
+
+            ccsv.flush()
+
+    def pull_request_info(self):
+        self.pull_requests = get_all_pull_requests(self.r)
+
+        with open('pull_requests.csv','w') as prcsv:
+            fieldnames = ['id','user','created_at','closed_at', 'additions', 'deletions', 'changed_files', 'review_comments','merged']
+            w = csv.DictWriter(prcsv, fieldnames)
+        
+            w.writeheader()
+            for i,pr in self.pull_requests.items(): #because id is a built-in function, we use the variable i
+                w.writerow({'id': i, 'user': pr['user'], 'created_at': pr['created_at'], 'closed_at' : pr['closed_at'], 'additions':pr['additions'], 'deletions':pr['deletions'], 'changed_files': pr['changed_files'], 'review_comments' : pr['review_comments'], 'merged' : pr['merged']})
+
+            prcsv.flush()
+
+if __name__ == "__main__":
+    
+    #Initiate API connection
+    g = Github(login_or_token=None,password=None) #Use your username and password for getting over rate limit.
+    
+    gi = GithubInfo(user='gayatrivenugopal', repo='MobileTechnologies') #Initate our Info gatherer
+
+    gi.forks_info()
+    gi.commits_info()
+    gi.pull_request_info()
--- utils.py
+++ utils.py
@@ -1,37 +1,45 @@
-import json
-import requests
-users = []
+import logging
+logger = logging.getLogger()
+x = logging.StreamHandler()
+logger.addHandler(x)
 
-class GitHub:
-    users = []  #users that we are concerned with.
-    repos = {}  #repos that we are concerned with, including forks
-    commits = [] #all commits that we are concerned with
+repos = {}
+commits = {}
+pull_requests = {}
 
-    def get_forks(self, url):
-        fs = requests.get(url)
-        fs_json = json.loads(fs.text or fs.content)
+def get_all_related_repos(r):
+    'retrieves all forks and any forks of these forks and so on of a given repo'
+    for r in r.get_forks():
+       repos[r.owner.name] = r #record
 
-        for f in fs_json:
-            print "Owner: ", f['owner']['login']
-            login = f['owner']['login']
-            self.users.append(login)
-            print "Fork : ", f['name'], f['url']
-            self.repos[login] = f['name']
-            if f['forks_count'] > 0:
-                print "This has been further forked...printing them"
-                self.get_forks(f['forks_url'])
+       if r.forks_count > 0:
+           print "This has been further forked...printing them"
+           get_all_related_repos(r)
 
-    def commits_on_repos(self, repos):
-        '''Expects `repos` to be a non-empty dictionary of owner:repo pairs'''
-        import pdb
-        pdb.set_trace()
-        for owner,repo in repos.items():
-            repo_url = "https://api.github.com/repos/%s/%s/commits" % (owner, repo)
-            commits = json.loads(requests.get(repo_url).text)
-            
-            for c in commits:
-                print c['commit']['tree']['sha']
+    return repos
+
+def get_all_commits_on_repos(repos):
+    '''Expects `repos` to be a non-empty dictionary of owner:repo pairs'''
+    for r in repos.values():
+        cs = r.get_commits()
+        for c in cs:
+            print "c", c
+            commits[c.sha] = {'author':c.author.name, 'committer' : c.committer.name, 'repo': r.name, 'repo_url' : r.url, 'date': c.commit.last_modified, 'comment': c.commit.message}
+            if c.author.name is None or c.author.name.strip() == '':
+                commits[c.sha]['author'] = c.author.login
+
+            if c.committer.name is None or c.committer.name.strip() == '':
+                commits[c.sha]['committer'] = c.committer.login
+
+    return commits
+
+def get_all_pull_requests(r):
+    '''Gets all pull requests for the repo r, regardless of whether they are open or closed'''
     
-    def get_commits_by_users(self, users):
-        '''Expects `users` to be a non-empty list of string login names.'''
-        #TODO: List all commits done by a user on ALL repos.
+    for p in r.get_pulls(state='all'):
+        pull_requests[p.id] = {'user': p.user.name, 'created_at': p.created_at, 'closed_at':p.closed_at, 'additions':p.additions, 'deletions':p.deletions, 'changed_files': p.changed_files, 'review_comments' : p.review_comments, 'merged' : p.is_merged()}
+
+        if p.user.name is None or p.user.name.strip() == '':
+            pull_requests[p.id]['user'] = p.user.login
+
+    return pull_requests
